7d00b34bf9982316045c79d9b0ca2a5eb97c7ece
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-19 18:11:27 +0530

    sdm660-common: disable hwc vds in surface flinger
    * Fixes jittery ui during screen recorder until vendor R realease is available
    
    Change-Id: I5f27374eb6a9fce26a3f17073e65fe31dcfc8e54

--- properties.mk
+++ properties.mk
@@ -152,7 +152,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.disable_backpressure=1 \
     debug.sf.enable_gl_backpressure=1 \
-    debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=0 \
     debug.sdm.support_writeback=0 \
