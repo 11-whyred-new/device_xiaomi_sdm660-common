7ab532f67805efa0eeb7d2fc8224d053846d5dbc
kubersharma001 <kubersharma001@gmail.com>
2021-10-13 08:12:52 +0000

    sdm660-common: Remove config_jitzygoteBootImagePinnerServiceFiles list
     - obsolete on android-12
    
    Ref: I97738435edd07e8b6770372cd935218c1a9cd984
    Change-Id: Ibbc8d35961d919ccdc454ef2043824f38191637e
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -157,7 +157,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0:64
+    vendor.qti.hardware.camera.device@1.0:64 \
+    libmm-qcamera
 
 # Component overrides
 PRODUCT_COPY_FILES += \
