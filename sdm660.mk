cbf5396424eae9af4d5bc830fef6bc44c666260a
y <y>
2021-10-14 20:43:47 +0700

    prebuilt apps

--- sdm660.mk
+++ sdm660.mk
@@ -474,7 +474,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# WiFi Display
+# Gallery
+PRODUCT_PACKAGES += \
+    SimpleGallery
+
+# Wifi-Display
 PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl \
