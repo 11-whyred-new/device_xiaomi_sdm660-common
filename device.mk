af07b6fe5063b77f6aa67125b0b41748b7a04e64
y <y>
2021-10-14 20:43:47 +0700

    prebuilt apps

--- device.mk
+++ device.mk
@@ -517,6 +517,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Gallery
+PRODUCT_PACKAGES += \
+    SimpleGallery
+
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
     libaacwrapper \
