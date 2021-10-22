47bc1021a267f72cfb144042de9fbb4700974c83
y <y>
2021-10-22 07:04:04 +0700

    fix

--- sdm660.mk
+++ sdm660.mk
@@ -31,6 +31,9 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI
 
+# IORAP
+include build/make/target/product/iorap_large_memory_config.mk
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
