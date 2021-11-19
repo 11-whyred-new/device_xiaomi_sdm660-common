b61324c84cbc1343054744b8037ef8370c6295fb
iqbal <whyred@ip-172-31-44-109.us-east-2.compute.internal>
2021-11-19 23:49:18 +0000

    Revert "cok"
    This reverts commit 6327568e06287f39e9ffa1eef0bd8904abb091d7.

--- properties.mk
+++ properties.mk
@@ -16,13 +16,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
-    vendor.audio_hal.period_size=192 \
-    audio.offload.min.duration.secs=30 \
+    vendor.audio_hal.period_size=240 \
     ro.vendor.audio.sdk.fluencetype=fluence \
     persist.audio.fluence.voicecomm=true \
     persist.vendor.audio.fluence.voicecall=true \
-    persist.vendor.audio.fluence.voicerec=false \
-    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicerec=true \
+    persist.vendor.audio.fluence.speaker=false \
     vendor.audio.tunnel.encode=false \
     persist.vendor.audio.ras.enabled=false \
     vendor.audio.offload.buffer.size.kb=64 \
