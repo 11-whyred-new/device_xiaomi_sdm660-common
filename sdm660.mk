6961dfa5a12cdf7d962f359ac05bfa3e50c595c0
iqbal <whyred@ip-172-31-44-109.us-east-2.compute.internal>
2021-11-21 23:24:31 +0000

    anu

--- sdm660.mk
+++ sdm660.mk
@@ -487,5 +487,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# mod
+PRODUCT_PACKAGES += \
+    dotOSModOverlay-SystemUI \
+    dotOSModOverlay-framework-res
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
