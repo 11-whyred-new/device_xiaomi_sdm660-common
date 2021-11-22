36b146eeeb085a5c88403e75615a17ca8c0bdb2c
iqbal <whyred@ip-172-31-44-109.us-east-2.compute.internal>
2021-11-22 14:35:39 +0000

    Revert "sdm660-common: Remove config_jitzygoteBootImagePinnerServiceFiles list"
    This reverts commit 2d955543817b2844c4c53869cba7367896a1444b.

--- sdm660.mk
+++ sdm660.mk
@@ -157,8 +157,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0:64 \
-    libmm-qcamera
+    vendor.qti.hardware.camera.device@1.0:64
 
 # Component overrides
 PRODUCT_COPY_FILES += \
