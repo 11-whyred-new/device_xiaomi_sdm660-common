8d9023aeb5738cf0990de04c4e0ad0aaf7b5ba33
zacky46 <kngsoft46@gmail.com>
2022-05-27 12:10:21 +0700

    anuim

--- sdm660.mk
+++ sdm660.mk
@@ -356,6 +356,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor
 
