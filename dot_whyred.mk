c240ddd98b78e7b3640e6e7b9f875826058222a7
Ubuntu <azure@CodenameAzure.yenprscofjienogsowdwa2adpb.rx.internal.cloudapp.net>
2022-08-02 12:57:22 +0000

    dotos

---
+++ dot_whyred.mk
@@ -0,0 +1,58 @@
+# 
+# Copyright (C) 2018 The Mokee Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+# Enable updating of APEXes
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Inherit from whyred device
+$(call inherit-product, device/xiaomi/whyred/device.mk)
+
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
+
+PRODUCT_NAME := dot_whyred
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := whyred
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi Note 5
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="whyred" \
+    PRODUCT_NAME="whyred" \
+    PRIVATE_BUILD_DESC="whyred-user $(PLATFORM_VERSION) $(BUILD_ID) $(shell date -u +%H%M) release-keys"
+
+BUILD_FINGERPRINT := xiaomi/whyred/whyred:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date -u +%H%M):user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
+
+TARGET_VENDOR := Xiaomi
+IS_PHONE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_SCREEN_DENSITY := 440
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
