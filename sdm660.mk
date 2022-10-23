fde0b861dd5098d7934ee7df684ee01adb3a6126
Ubuntu <zeus@neutron.pnkzo5atxdhupdwioxqrnfjp2g.ix.internal.cloudapp.net>
2022-10-23 22:55:13 +0000

    RRO

--- sdm660.mk
+++ sdm660.mk
@@ -8,7 +8,10 @@
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# RRO (Runtime Resource Overlay)
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
