eaaabefe41ce4b32e51c4b6559660eb08e5539d8
Ubuntu <zeus@neutron.pnkzo5atxdhupdwioxqrnfjp2g.ix.internal.cloudapp.net>
2022-11-05 13:10:16 +0000

    Revert "camera"
    This reverts commit 12ccf2c0106193d486c1aa1a95b173f72a48ae3b.

--- sdm660.mk
+++ sdm660.mk
@@ -154,9 +154,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.cameraservice.common@2.0.vendor \
-    android.frameworks.cameraservice.device@2.0.vendor \
-    android.frameworks.cameraservice.service@2.1.vendor \
     android.hardware.camera.device@3.5:64 \
     android.hardware.camera.provider@2.6:64 \
     android.hardware.camera.provider@2.6 \
@@ -164,14 +161,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0:64 \
-    libcamera2ndk_vendor \
-    libdng_sdk \
-    libdng_sdk.vendor \
-    libgui_vendor:32 \
-    libgrallocusage.vendor \
-    libstdc++ \
-    libstdc++.vendor
+    vendor.qti.hardware.camera.device@1.0:64
 
 # Component overrides
 PRODUCT_COPY_FILES += \
